d3dbe7fe4beef52e4bd0e1670adc028be21ea376
Alex Weber <weberalexanderj@gmail.com>
2018-03-04 18:31:56 -0600

    Initial push

---
+++ NM_Demo/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from NM_Demo.forms import CardDisplayForm
+
+import mtgsdk
+from mtgsdk import Card
+from mtgsdk import Set
+from mtgsdk import Type
+from mtgsdk import Supertype
+from mtgsdk import Subtype
+from mtgsdk import Changelog
+
+
+def CardListView(request, pk):
+    if request.method == "POST":
+        form = CardDisplayForm(request.POST)
+        if form.is_valid():
+            keywords = form.cards
+            return HttpResponseRedirect(reverse('/'))
+    else:
+        form = CardDisplayForm()
+
+    return render(request, "index.html", )
+
+
+def index(request):
+    # if request.method == "POST":
+    search_term=request.POST.get('keywords')
+    #search_term="Bolt"
+    cards = Card.where(name=search_term).all()
+    card_query = cards
+    form = CardDisplayForm()
+    context = {'cards': card_query, 'form': form }
+    if search_term is None:
+        context = {}
+    return render(request, 'index.html', context)
+
